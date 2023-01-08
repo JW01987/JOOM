7ea8f77b2e944f99e192e8583cfdef5a0e712ce4
Jean <etnpfj@gmail.com>
2023-01-08 17:29:50 +0900

    ws로 채팅 구현하기

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,38 @@
-console.log('Rb')
+const msgForm = document.querySelector('#msg')
+const nickForm = document.querySelector('#nick')
+const msgList = document.querySelector('ul')
+const socket = new WebSocket(`ws://${window.location.host}`)
+
+function makeJson(type, payload) {
+    const data = {
+        type: type,
+        payload: payload
+    }
+    return JSON.stringify(data)
+}
+
+socket.addEventListener('open', () => {
+    console.log('Server is Open ✅')
+})
+
+socket.addEventListener('message', (message) => {
+    const li = document.createElement('li')
+    li.innerText = message.data
+    msgList.append(li)
+})
+
+socket.addEventListener('close', () => {
+    console.log('Server is Close ❌')
+})
+
+msgForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const input = msgForm.querySelector('input')
+    socket.send(makeJson('message', input.value))
+    input.value = ''
+})
+nickForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const input = nickForm.querySelector('input')
+    socket.send(makeJson('nickname', input.value))
+})
--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
+import { parse } from 'path';
 import WebSocket, { WebSocketServer } from 'ws';
 
 const express = require('express')
 const app = express()
 const port = 3000
+const http = require("http");
 const server = http.createServer(app)
 const wss = new WebSocketServer({ server })
 
@@ -11,11 +13,30 @@ app.set("views", __dirname + "/views")
 app.use('/public', express.static(__dirname + "/public"))
 
 
-
 app.get('/', (req, res) => {
     res.render('home')
 })
 
+
+const sockets = []
+wss.on("connection", (socket) => {
+    sockets.push(socket)
+    socket['nickname'] = "Anon"
+    console.log('Server is Open ✅')
+    socket.on('message', (msg) => {
+        const parseMsg = JSON.parse(msg)
+        switch (parseMsg.type) {
+            case "message":
+                sockets.forEach((a) => { a.send(`${socket.nickname}: ${parseMsg.payload.toString()}`) })
+            case "nickname":
+                socket['nickname'] = parseMsg.payload
+        }
+
+    })
+    socket.on('close', () => {
+        console.log('Server is Close ❌')
+    })
+})
 
 server.listen(port, () => {
     console.log(`${port}에 서버 시작`)
